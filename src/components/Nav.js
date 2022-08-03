ddd2477b3246f4e611a6899a004f7db5fffc3a65
bluepebble25 <bluepebble25@gmail.com>
2022-08-03 19:14:47 +0900

    feat(Nav.js): 스크롤 내리는 정도에 따라 Navbar 배경색 변하는 기능 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.css';
 
 function Nav() {
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 50) {
+        setIsShown(true);
+      } else {
+        setIsShown(false);
+      }
+    });
+  
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    }
+  }, [])
+  
+
   return (
-    <nav className="nav nav--color-black">
+    <nav className={`nav ${isShown && "nav--color-black"}`}>
       <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
