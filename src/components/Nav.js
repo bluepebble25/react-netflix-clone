ccb4a42e8f5b8ff449c8e21904c1138319d51c32
bluepebble25 <bluepebble25@gmail.com>
2022-08-09 15:10:07 +0900

    feat: 검색창 및 검색결과 fetch 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './Nav.css';
+import { useNavigate } from 'react-router-dom';
 
 function Nav() {
   const [isShown, setIsShown] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -16,8 +19,12 @@ function Nav() {
     return () => {
       window.removeEventListener('scroll', () => {});
     }
-  }, [])
-  
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
 
   return (
     <nav className={`nav ${isShown && "nav--color-black"}`}>
@@ -27,6 +34,16 @@ function Nav() {
         className="nav__logo"
         onClick={() => window.location.reload()}
         />
+
+      <input
+        type="text"
+        value={searchValue}
+        size="30"
+        onChange={handleChange}
+        className="nav__search"
+        placeholder="검색하실 작품을 입력하세요."
+      />
+
       <img
         alt="User logged"
         src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from '../../api/axios';
+import { useLocation } from 'react-router-dom';
 
 function Searchpage() {
+  const [searchResults, setSearchResults] = useState([]);
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if(searchTerm) {
+      fetchSearchedMovie(searchTerm);
+    }
+  }, [searchTerm]);
+  
+  const fetchSearchedMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      console.log(request);
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
     <div></div>
   )
