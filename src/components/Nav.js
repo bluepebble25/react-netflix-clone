e9c95a614eb68760d1f8740ab9a133d762c2300e
bluepebble25 <bluepebble25@gmail.com>
2022-08-18 19:14:50 +0900

    feat(Nav): 검색 아이콘 클릭하면 검색바가 나타나게 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './Nav.css';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import useOnClickOutside from '../hooks/useOnClickOutside';
+import { ReactComponent as SearchIcon } from '../assets/search-icon.svg';
+import { useRef } from 'react';
 
 function Nav() {
   const [isShown, setIsShown] = useState(false);
   const [searchValue, setSearchValue] = useState("");
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
   const navigate = useNavigate();
+  const searchbarRef = useRef();
+
+  useOnClickOutside(searchbarRef, () => {
+    setIsSearchFocused(false);
+  })
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -28,27 +37,41 @@ function Nav() {
 
   return (
     <nav className={`nav ${isShown && "nav--color-black"}`}>
-      <img
-        alt="Netflix logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
-        className="nav__logo"
-        onClick={() => window.location.reload()}
+      <Link to="/">
+        <img
+          alt="Netflix logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+          className="nav__logo"
         />
+      </Link>
 
-      <input
-        type="text"
-        value={searchValue}
-        size="30"
-        onChange={handleChange}
-        className="nav__search"
-        placeholder="검색하실 작품을 입력하세요."
-      />
+      <div className={`nav__right-items ${isSearchFocused && "search-focused"}`}>
+        <div className="search">
+          <span
+            className="search__search-icon"
+            onClick={() => setIsSearchFocused(true)}
+          >
+            <SearchIcon width="24" height="24" />
+          </span>
+          <input
+            ref={searchbarRef}
+            type="text"
+            value={searchValue}
+            size="30"
+            onChange={handleChange}
+            className={`search__searchbar ${isSearchFocused && "search__bar--focused"}`}
+            placeholder="검색하실 작품을 입력하세요."
+          />
+        </div>
 
-      <img
-        alt="User logged"
-        src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
-        className="nav__avatar"
-      />
+        <img
+          alt="User logged"
+          src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+          className="nav__avatar"
+        />
+      </div>
+
+      
     </nav>
   )
 }
